9701e655f4cb13860952037a8eedf67191ece65d
daouloccol88 <Daouda@iMac-de-codeloccol-83.local>
2020-06-18 04:02:27 +0100

    Updates

---
+++ precache-manifest.ea3707d2d3b368eb8802c22ff12fd0a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47a295559cdd390375f3fc7198aab0f3",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "e4f75644303547f4d0b4",
+    "url": "/react-todo-list/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c692d7e382f5e8356b59",
+    "url": "/react-todo-list/static/css/main.7f0e7335.chunk.css"
+  },
+  {
+    "revision": "e4f75644303547f4d0b4",
+    "url": "/react-todo-list/static/js/2.97704e0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.97704e0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c692d7e382f5e8356b59",
+    "url": "/react-todo-list/static/js/main.16368c9d.chunk.js"
+  },
+  {
+    "revision": "345ac7126bf722b6fb11",
+    "url": "/react-todo-list/static/js/runtime-main.1b4aeefe.js"
+  }
+]);
